dc6ae8e3a7b5b429fc62b674b1eeea0bc779debd
chris <cj1098silver@gmail.com>
2014-12-25 14:25:25 -0600

    able to connect to hummingbird v1 API

--- app/src/main/java/cj1098/animeshare/MainActivity.java
+++ app/src/main/java/cj1098/animeshare/MainActivity.java
@@ -1,9 +1,20 @@
 package cj1098.animeshare;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +23,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        task task = new task();
+        task.execute();
     }
 
 
@@ -35,5 +48,43 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getData() {
+
+
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+
+            HttpGet request = new HttpGet();
+            URI website = new URI("https://hummingbirdv1.p.mashape.com/anime/steins-gate");
+            request.setHeader("X-Mashape-Key", "rasJF18hhHmshDKpDzwpvlmZt5rAp1YrLFdjsn2XGCcBALFoQy");
+            request.setURI(website);
+            HttpResponse response = httpclient.execute(request);
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent()));
+
+            String line = in.readLine();
+
+            Log.d("AHSHDA", line);
+
+        }catch(Exception e){
+            Log.e("log_tag", "Error in http connection " + e.toString());
+        }
+    }
+
+
+    public class task extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            getData();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
 }
